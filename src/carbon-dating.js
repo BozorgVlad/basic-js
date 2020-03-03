90e61883604d65ff630dac769dac19caefb59975
BozorgVlad <bozorgvlad@gmail.com>
2020-03-03 18:47:39 +0300

    feat: add solution for carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,10 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let num = parseFloat(sampleActivity);
+  if (typeof sampleActivity != 'string' || num > MODERN_ACTIVITY || isNaN(num) || 0 >= num) {
+    return false;
+  }; 
+  return Math.ceil( Math.log( MODERN_ACTIVITY / num ) / (0.693 / HALF_LIFE_PERIOD ));
 };
