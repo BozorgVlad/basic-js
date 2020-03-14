1e42435ee83c9089a1e6ee0a0adc7e0522216c36
BozorgVlad <bozorgvlad@gmail.com>
2020-03-15 01:49:45 +0300

    feat: add solution for recursive depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,8 +2,13 @@ module.exports = class DepthCalculator {
     calculateDepth(arr) {
         let depth = 0;
         if (Array.isArray(arr)) {
+            let count = 0;
             
+            for (let i = 0; i < arr.length; i++) {
+             count = Math.max(this.calculateDepth(arr[i]), count);
+            }
+            depth = count + 1;
         }
-      
+      return depth;
     }
 };
